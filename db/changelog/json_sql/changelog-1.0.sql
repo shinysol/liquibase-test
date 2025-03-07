4d43c6a36753dd3e84fb2643dc83ac3757ea84e1
Park Hyunwoo <hyunwoo.park@izex.co.kr>
2025-03-07 16:19:14 +0900

    rollback

--- db/changelog/json_sql/changelog-1.0.sql
+++ db/changelog/json_sql/changelog-1.0.sql
@@ -1,9 +1,7 @@
 --liquibase formatted sql
 
 --changeset author:changelog-1.0-1
-CREATE DATABASE IF NOT EXISTS test;
-USE test;
-CREATE TABLE IF NOT EXISTS test.users (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL
