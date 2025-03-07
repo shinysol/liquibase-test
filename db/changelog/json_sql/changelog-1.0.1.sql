015e7f2aceabb700fb2d78d06a0b1114e4b68293
Park Hyunwoo <hyunwoo.park@izex.co.kr>
2025-03-07 16:46:17 +0900

    update 1.0.1

--- db/changelog/json_sql/changelog-1.0.1.sql
+++ db/changelog/json_sql/changelog-1.0.1.sql
@@ -1,4 +1,8 @@
 --liquibase formatted sql
 
 --changeset author:changelog-1.0.1-1
-INSERT INTO users VALUES (1, 'david', 'david@test.com');
+ALTER TABLE `users`	CHANGE COLUMN `id` `id` INT NOT NULL AUTO_INCREMENT FIRST;
+
+--changeset author:changelog-1.0.1-2
+INSERT INTO users (username, email) VALUES ('david', 'david@test.com');
+
